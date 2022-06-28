74e383c1c75522800c2af774790dfc1dfdb9f015
Aymen <AymenMir1001@gmail.com>
2022-06-28 22:37:47 +0100

    Delete src directory

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Post } from "./Post";
-export { default as CreatePost } from "./CreatePost";
-export { default as AuthorDashboard } from "./AuthorDashboard";
--- src/utils/contracts-config.js
+++
@@ -1,5 +0,0 @@
-
-  export const contractAddress = "0xB1474B51574f9B3E30fe9C1189261ca3F04a5cb2"
-  export const ownerAddress = "0x5F7C66C21F3A82DEF71Af17bcF863d17717D8dBB"
-  export const networkDeployedTo = "1337"
-  
