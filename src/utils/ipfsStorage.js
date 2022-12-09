2c504eea71950393857178e89651ec14f85f3287
Aymen <AymenMir1001@gmail.com>
2022-12-09 10:57:02 +0100

    Create ipfsStorage.js

---
+++ src/utils/ipfsStorage.js
@@ -0,0 +1,21 @@
+import { Web3Storage } from "web3.storage";
+
+const web3storage_key = "YOUR-WEB3.STORAGE-API-TOKEN";
+
+export const IPFS_GATEWAY = "https://ipfs.io/ipfs/";
+
+function GetAccessToken() {
+  return web3storage_key;
+}
+
+function MakeStorageClient() {
+  return new Web3Storage({ token: GetAccessToken() });
+}
+
+export const ipfsSaveContent = async (files) => {
+  console.log("Uploading files to IPFS with web3.storage....");
+  const client = MakeStorageClient();
+  const cid = await client.put([files]);
+  console.log("Stored files with cid:", cid);
+  return cid;
+};
